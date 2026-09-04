5b37b95092502e1eacfe86c2b0f3249644dcc28f
AmanPathak198 <aman.pathak.198@gmail.com>
2026-09-04 23:38:01 +0530

    PROJ-102: verify CI blocks failing tests (temporary)

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, world!"}
+    assert response.json() == {"message": "Wrong Message"}
 
 def test_health_check():
     response = client.get("/health")
